996e178139bd85386a18821bfbbe856eb5e94209
horzwxy <wangxiayang.fdt@gmail.com>
2013-09-25 09:19:45 +0800

    modify project structure

--- gen/cn/naviro/tool/sumsungblerecsingle/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package cn.naviro.tool.sumsungblerecsingle;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
